c4e1088aac290b752a36be7b28d889f0999995f9
gomzyakov <alexander.gomzyakov@gmail.com>
2019-09-15 09:29:14 +0500

    Add CI YML-files

--- tests/Test.php
+++ tests/Test.php
@@ -17,12 +17,12 @@ class Helper_StringToColor_Test extends PHPUnit\Framework\TestCase
 
     public function test_Convert_short_string_to_color()
     {
-        $color = \Gomzyakov\StringToColor::convertStringToColor('A');
+        $color = \Gomzyakov\StringToColor::convertStringToColor('Sasha');
 
         $expected_color = [
-            'red'   => 90,
-            'green' => 117,
-            'blue'  => 38,
+            'red'   => 170,
+            'green' => 79,
+            'blue'  => 45,
         ];
 
         $this->assertEquals($expected_color, $color);
